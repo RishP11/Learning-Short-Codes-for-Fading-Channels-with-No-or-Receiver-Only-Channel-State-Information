3f89531db5358c92548625cd4a1332366051f4f6
Rishabh Pomaje <97334879+RishP11@users.noreply.github.com>
2024-09-16 18:40:59 +0530

    Latest models and results code

--- SISO_noCSI_branch_diversity/run_script.py
+++ SISO_noCSI_branch_diversity/run_script.py
@@ -7,4 +7,5 @@ n = 2
 # deepSISOnoCSI_bd.branch_diversity_routine(k, n, 10, 'rayleigh')
 # deepSISOnoCSI_bd.branch_diversity_routine(k, n, 10, 'gamma')
 # deepSISOnoCSI_bd.branch_diversity_routine(k, n, 10, 'gumbel')
-deepSISOnoCSI_bd.branch_diversity_routine(k, n, 10, 'custom')
+# deepSISOnoCSI_bd.branch_diversity_routine(k, n, 10, 'custom')
+deepSISOnoCSI_bd.branch_diversity_routine(k, n, 10, 'foldedNormal')
